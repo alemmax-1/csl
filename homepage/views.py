b1b63ab8b5c8e0e8d55973000f7e00c907485d40
Alessandro Matteucci <alemmax@gmail.com>
2025-04-17 06:48:29 +0000

    Aggiunta la struttura della home page con il modello Carousel, inclusi i template e le configurazioni necessarie per la navigazione e il cambio di tema.

--- homepage/views.py
+++ homepage/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import Carousel
 # Create your views here.
+def index(request):
+    # Recupera gli oggetti del modello Carousel
+    carousel_items = Carousel.objects.all()
+    
+    # Passa gli oggetti al template
+    context = {
+        'carousels': carousel_items,
+    }
+    
+    return render(request, 'homepage/index.html', context)
