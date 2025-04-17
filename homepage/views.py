cbd526d703fa194138c09619cdd97ddb6c9552c0
Alessandro Matteucci <alemmax@gmail.com>
2025-04-17 15:15:06 +0000

    Aggiunta la gestione degli oggetti di marketing nella vista principale per il template index.html

--- homepage/views.py
+++ homepage/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-from .models import Carousel
+from .models import Carousel, Marketing
 # Create your views here.
 def index(request):
     # Recupera gli oggetti del modello Carousel
     carousel_items = Carousel.objects.all()
-    
+    # Recupera gli oggetti del modello Marketing
+    marketing_items = Marketing.objects.all()
     # Passa gli oggetti al template
     context = {
         'carousels': carousel_items,
+        'marketings': marketing_items,
     }
     
     return render(request, 'homepage/index.html', context)
