2ff4cab6f7e564a70d5a010211d923ee041e505d
Alessandro Matteucci <alemmax@gmail.com>
2025-04-17 15:14:13 +0000

    Ristrutturato il modello Carousel per estendere AbstractContent e migliorare la gestione dei campi condivisi.

--- homepage/models.py
+++ homepage/models.py
@@ -4,18 +4,33 @@ from cloudinary.models import CloudinaryField
 # Create your models here.
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-class Carousel(models.Model):
+class AbstractContent(models.Model):
+    """
+    Abstract model for shared carousel fields.
+    """
+    image = CloudinaryField('image', default='placeholder')
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    order = models.IntegerField(default=0)
+
+    class Meta:
+        abstract = True
+        
+class Carousel(AbstractContent):
     """
     Modello per la gestione delle immagini del carosello.
     """
     # image = models.ImageField(upload_to='carousel_images/')
-    image = CloudinaryField('image', default='placeholder')
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    order = models.IntegerField(
-        default=0, unique=True,
-        validators=[MinValueValidator(0), MaxValueValidator(4)]
-        )
+    order = models.IntegerField(default=0, unique=True)
 
+    def __str__(self):
+        return f"{self.title} - {self.order}"
+
+
+class Marketing(AbstractContent):
+    """
+    Modello per la gestione delle immagini del marketing.
+    """
+    order = models.IntegerField(default=0, unique=True, validators=[MinValueValidator(0), MaxValueValidator(2)])
     def __str__(self):
         return f"{self.title} - {self.order}"
